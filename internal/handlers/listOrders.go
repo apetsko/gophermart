7e12d4aa2309f2cb7245167f6cc6e96d6732fe68
Maksim Apetsko <apetskom@gmail.com>
2025-02-19 23:25:48 +0500

    addOrder listOrders handlers init

---
+++ internal/handlers/listOrders.go
@@ -0,0 +1,67 @@
+package handlers
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"time"
+)
+
+func ListOrdersHandler(h *URLHandler) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		_, cancel := context.WithTimeout(r.Context(), time.Second*3)
+		defer cancel()
+		defer r.Body.Close()
+
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "", http.StatusBadRequest)
+			return
+		}
+
+		//var user models.User
+		//err = json.Unmarshal(b, &user)
+		//if err != nil {
+		//	http.Error(w, "", http.StatusBadRequest)
+		//	return
+		//}
+		//
+		//validate := validator.New()
+		//if err = validate.Struct(user); err != nil {
+		//	validationErrors := err.(validator.ValidationErrors)
+		//	h.logger.Info(fmt.Sprintf("%v", validationErrors))
+		//	http.Error(w, "", http.StatusBadRequest)
+		//	return
+		//}
+		//
+		//ue, err := h.storage.GetUser(ctx, user.Login)
+		//
+		//if err != nil {
+		//	if errors.Is(err, models.ErrUserNotFound) {
+		//		h.logger.Error(err.Error())
+		//		http.Error(w, err.Error(), http.StatusUnauthorized)
+		//		return
+		//	}
+		//	return
+		//}
+		//
+		//if ok := auth.ComparePassword(ue.PasswordHash, user.Password); !ok {
+		//	http.Error(w, "", http.StatusUnauthorized)
+		//	return
+		//}
+		//
+		//err = auth.SetUserIDCookie(w, ue.ID, h.secret)
+		//if err != nil {
+		//	return
+		//}
+
+		//h.logger.Info("registered user", "user", user)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		_, err = w.Write(b)
+		if err != nil {
+			return
+		}
+	}
+}
